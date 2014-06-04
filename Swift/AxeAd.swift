efefef16782d002d20cb1f3238a365354dacbee1
Martin <m.hatzung@gmail.com>
2014-06-04 22:18:07 +0200

    Added swift

---
+++ Swift/AxeAd.swift
@@ -0,0 +1,19 @@
+class AxeAd {
+    var a = A()
+}
+class A {
+    var this = 1
+    //var girlfriend = 42
+    func girlfriend()  {
+        println("NO!")
+    }
+}
+var you = 1
+var understand = A()
+var get = AxeAd()
+
+/************************************/
+if (you == understand.this) {
+    get.a.girlfriend();
+}
+/************************************/
